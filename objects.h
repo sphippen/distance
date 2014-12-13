1a1c402d0f7b78522bb56af4fbe14e5cd96067ee
Spencer Phippen <spencer.phippen@gmail.com>
2014-12-13 02:15:24 -0700

    Added sphere object

--- objects.h
+++ objects.h
@@ -1,4 +1,3 @@
-//! 
 //! objects.h
 //! Author: Spencer Phippen
 //! zlib/libpng licensed (see LICENSE)
@@ -12,6 +11,18 @@ namespace distance {
 
 class Object {
   virtual double minDistance(Vec3 p) = 0;
+};
+
+class Sphere : public Object {
+ public:
+  Vec3 center;
+  double radius;
+
+  Sphere() : center(0.0), radius(1.0) {}
+  Sphere(Vec3 center, double radius) : center(center), radius(radius) {}
+  virtual double minDistance(Vec3 p) override {
+    return (p-center).length();
+  }
 };
 
 }
